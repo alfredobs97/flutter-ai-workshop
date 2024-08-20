8e4c9e39219cf72f78772b0f6624772c94db4bd2
Alfredo <alfredobautista1@gmail.com>
2024-08-20 19:47:38 +0200

    implement interface and clean up

--- lib/data/gemini_service.dart
+++ lib/data/gemini_service.dart
@@ -1,51 +1,45 @@
 import 'dart:typed_data';
 
-import 'package:flutter_ai_workshop/data/vertex_http_client.dart';
+import 'package:flutter_ai_workshop/domain/ai_service.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_generative_ai/google_generative_ai.dart';
 
-class GeminiService {
+class GeminiService extends AiService {
   final GenerativeModel _model;
-  final GenerativeModel _visionModel;
 
   GeminiService()
       : _model = GenerativeModel(
-          model: 'gemini-pro',
-          apiKey: dotenv.env['GEMINI_API_KEY']!,
-        ),
-        _visionModel = GenerativeModel(
-          model: 'gemini-pro-vision',
+          model: 'gemini-1.5-flash',
           apiKey: dotenv.env['GEMINI_API_KEY']!,
         );
 
-  GeminiService.vertexAi({
-    required String projectUrl,
-  })  : _model = GenerativeModel(
-          model: 'gemini-pro',
-          apiKey: dotenv.env['VERTEX_API_KEY']!,
-          httpClient: VertexHttpClient(projectUrl),
-        ),
-        _visionModel = GenerativeModel(
-          model: 'gemini-pro-vision',
-          apiKey: dotenv.env['VERTEX_API_KEY']!,
-          httpClient: VertexHttpClient(projectUrl),
-        );
-
-  Future<String?> generateText(String prompt) async {}
-
-  Future<List<String?>> generateMultiplesReponses(String prompt) async {
-    return [];
+  @override
+  Future<void> init() {
+    // TODO: implement init
+    throw UnimplementedError();
   }
 
-  Future<String> describeImage(Uint8List imageInBytes) async {
-    return 'No description found for this image, try again!';
+  @override
+  Future<String?> generateContent(String prompt) {
+    // TODO: implement generateContent
+    throw UnimplementedError();
   }
 
-  Future<String?> guessLocationOfImage(Uint8List imageInBytes) async {
-    return 'No location found!';
+  @override
+  Future<String?> describeImage(Uint8List imageInBytes) {
+    // TODO: implement describeImage
+    throw UnimplementedError();
   }
 
+  @override
+  Future<String?> guessLocationOfImage(Uint8List imageInBytes) {
+    // TODO: implement guessLocationOfImage
+    throw UnimplementedError();
+  }
+
+  @override
   Stream<String?> generateTextStream(String prompt) {
-    return const Stream.empty();
+    // TODO: implement generateTextStream
+    throw UnimplementedError();
   }
 }
